eec0028dfd3b2bf0423b7cbd77ec19a18a98c451
chaewon0228 <s2033@e-mirim.hs.kr>
2022-05-30 14:46:48 +0900

    basic class: List 클래스 (#2)

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -25,7 +25,24 @@ namespace CSClass
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble() * 10); // 큰 수를 반환
+
+            /*
+                        List<int> list = new List<int>();
+                        list.Add(1);
+                        list.Add(2);
+                        list.Add(4);
+                        list.Add(8);*/
+
+            List<int> list = new List<int>() { 1, 2, 4, 8 };
+            list.Remove(2);
+            list.RemoveAt(1);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\t" + item);
+            }
+
         }
     }
-   
+
 }
