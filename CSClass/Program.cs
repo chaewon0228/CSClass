bb7299b226d0ed6ddc1e9521e073f15c71e92898
chaewon0228 <s2033@e-mirim.hs.kr>
2022-06-07 11:44:59 +0900

    22-4. 오버로딩 #10

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -119,6 +119,8 @@ namespace CSClass
             Console.WriteLine(MyMath.Abs(-52));
             Console.WriteLine(MyMath.Abs(-52.3));
 
+            Console.WriteLine(MyMath.Abs(52f));
+            Console.WriteLine(MyMath.Abs(52L));
         }
     }
 }
